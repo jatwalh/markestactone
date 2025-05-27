03dc56045f377abde5ad16e9cd49742c370ede37
jatwalh <jatwalh@gmail.com>
2025-05-27 22:40:27 +0530

    2ndyrtydwerf

---
+++ controllers/airports/airports.controller.ts
@@ -0,0 +1,11 @@
+import { Airport } from "../../models/airports/airports.model";
+
+
+const create = (req, res)=>{
+    try{    
+
+    } catch(err){
+        console.log(err)
+        res.status(500).json({err : err.message})
+    }
+}
